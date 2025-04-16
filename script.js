3b40ca611b2eaa874a8268ac32c1bf14340883a8
sachin7767y <care@zenclean.solutions>
2025-04-16 23:11:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Add animations on scroll
-document.addEventListener("DOMContentLoaded", function () {
-  const serviceItems = document.querySelectorAll(".service-item");
-
-  serviceItems.forEach((item, index) => {
-    item.style.animationDelay = ${index * 0.2}s;
-  });
-});
